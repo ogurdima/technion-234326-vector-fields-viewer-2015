d00b5a08442ed880f453abd4c518b6add1044787
ogurdima <ogurdima@gmail.com>
2015-03-28 15:35:48 +0300

    Added cleanup script, changed projects intermediate paths

---
+++ cleanup.bat
@@ -0,0 +1,17 @@
+rmdir /Q /S Debug
+rmdir /Q /S Release
+
+rmdir /Q /S ManagedDll\Debug
+rmdir /Q /S ManagedDll\Release
+
+rmdir /Q /S MeshViewer\Debug
+rmdir /Q /S MeshViewer\Release
+
+rmdir /Q /S OpenMesh\Debug
+rmdir /Q /S OpenMesh\Release
+
+rmdir /Q /S VectorFieldsViewer\Debug
+rmdir /Q /S VectorFieldsViewer\Release
+
+rmdir /Q /S ParameterWindow\bin
+rmdir /Q /S ParameterWindow\obj
